9a364dee6bd11a75737a4720b0d47c3d22c3b366
QingWei <l.q.wei91@gmail.com>
2016-05-09 23:30:07 +0800

    Style plus button

--- src/blogpost/update/NewPostButton.js
+++ src/blogpost/update/NewPostButton.js
@@ -9,7 +9,7 @@ export default class NewPostButton extends Component {
     const { actions, postDraft, showEditor, showButton } = this.props
     if (!showButton) return null
     return (
-      <div>
+      <div id="new-post-button">
         <PostEditorPopUp
           show={showEditor}
           closePopUp={actions.hideNewPostEditor}
@@ -17,7 +17,11 @@ export default class NewPostButton extends Component {
           updatePostDraft={actions.updateNewPostDraft}
           submitChange={actions.createPostRequested}
         />
-        <button {...this.props} onClick={actions.showNewPostEditor}>New post</button>
+        <button {...this.props} onClick={actions.showNewPostEditor}>
+          <svg viewBox="0 0 32 32">
+            <path className="plus-button" d="M28,14H18V4c0-1.104-0.896-2-2-2s-2,0.896-2,2v10H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h10v10c0,1.104,0.896,2,2,2  s2-0.896,2-2V18h10c1.104,0,2-0.896,2-2S29.104,14,28,14z" />
+          </svg>
+        </button>
       </div>
     )
   }
