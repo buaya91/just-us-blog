139f5473721fa766cb8eb2d0fb9fe8e0565c0010
QingWei <l.q.wei91@gmail.com>
2016-05-10 22:05:58 +0800

    Add login route

--- src/top_level/routes.js
+++ src/top_level/routes.js
@@ -1,7 +1,9 @@
 import HomepageContainer from '../homepage/HomepageContainer'
+import ShowLoginPopUp from '../login/ShowLoginPopUp'
 import FilteredBlogPostList from '../blogpost/list/FilteredBlogPostList'
 
 export default [
   { path: '/', component: HomepageContainer },
+  { path: '/login', component: ShowLoginPopUp },
   { path: '/post', component: FilteredBlogPostList },
 ]
--- src/top_level/store.js
+++ src/top_level/store.js
@@ -1,6 +1,7 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import { createMiddleware, createLoader } from 'redux-storage'
 import createEngine from 'redux-storage-engine-localstorage'
+import filter from 'redux-storage-decorator-filter'
 import createSagaMiddleware from 'redux-saga'
 import reducers from './reducers'
 import sagas from './sagas'
@@ -13,7 +14,8 @@ const sagaMiddleware = createSagaMiddleware(
 )
 
 const localStorageEngine = createEngine('just-us')
-const localStorageMiddleware = createMiddleware(localStorageEngine)
+const filteredEngine = filter(localStorageEngine, [], ['login', 'showPopUp'])
+const localStorageMiddleware = createMiddleware(filteredEngine)
 
 const devtoolCreateStore = compose(
   applyMiddleware(sagaMiddleware, localStorageMiddleware),
